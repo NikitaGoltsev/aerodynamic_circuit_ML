8c31e5d8c49a822f835feb285e48e7f5b7370efa
RayanGosling <n.goltsev@g.nsu.ru>
2024-01-15 10:35:59 +0700

    Before_rms_metrik

--- copy_of_keras_nn.py
+++ copy_of_keras_nn.py
@@ -29,3 +29,27 @@ y = df['frequancy']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 20)
 
+from sklearn.preprocessing import StandardScaler
+scaler=StandardScaler()
+scaler.fit(X_train)
+
+X_train_scaled = scaler.transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+
+# Model
+
+model = Sequential()
+model.add(Dense(128, input_dim=13, activation='relu'))
+model.add(Dense(64, activation='relu'))
+#Output layer
+model.add(Dense(1, activation='linear'))
+
+model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mae'])
+model.summary()
+
+history = model.fit(X_train_scaled, y_train, validation_split=0.2, epochs =100)
+# Model
+
+
+
